72e25fa6a04319011c545a40aedfacb1f6050c00
rnichola <devraymond24@gmail.com>
2026-06-18 11:16:36 +0100

    feat: Add property listings and better error handling

--- shoplite/handlers/handlers.go
+++ shoplite/handlers/handlers.go
@@ -14,34 +14,53 @@ type appData struct {
 }
 
 var Listings []appData
+var tmpl *template.Template
+
+func init() {
+	var err error
+	tmpl, err = template.ParseFiles("public/index.html")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func AppHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.FormValue("id")
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Error parsing form", http.StatusInternalServerError)
+		return
+	}
+
 	name := r.FormValue("name")
 	item := r.FormValue("item")
 	price := r.FormValue("price")
 	des := r.FormValue("des")
-
-	idCount := len(id) + 1
 
 	if name == "" && item == "" && price == "" && des == "" {
 		http.Error(w, "All fields must not be empty", http.StatusBadRequest)
 		return
 	}
 
-	temp, err := template.ParseFiles("public/index.html")
-	if err != nil {
-		http.Error(w, "Frontend page missing", http.StatusNotFound)
-		return
-	}
-
-	data := appData{
-		ID:          idCount,
+	newData := appData{
+		ID:          len(Listings) + 1,
 		Name:        name,
 		Item:        item,
 		Price:       price,
 		Description: des,
 	}
-	temp.Execute(w, data)
-	Listings = append(Listings, data)
+	Listings = append(Listings, newData)
+
+	data := struct {
+		Listings []appData
+		NewItem  appData
+	}{
+		Listings: Listings,
+		NewItem:  newData,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error rendring template", http.StatusInternalServerError)
+		return
+	}
 }
