12b82c6e79f41bd0604ab2c4ea0ed52bb080d74d
rnichola <devraymond24@gmail.com>
2026-06-24 14:32:16 +0100

    ascii switcher done

--- l2e/ascii-web/main.go
+++ l2e/ascii-web/main.go
@@ -20,6 +20,7 @@ func main() {
 	mux.HandleFunc("/", handlers.HomeHanler)
 	mux.HandleFunc("/health", health)
 	mux.HandleFunc("POST /ascii-art", handlers.AsciiHandler)
+	mux.HandleFunc("/ascii-switch", handlers.SwitchHandler)
 
 	log.Println("Server running on http://127.0.0.1:8000")
 	err := http.ListenAndServe(":8000", mux)
