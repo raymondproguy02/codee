532f4ac36df1c14eba6ac7a49f55a8a2b3e0050a
rnichola <devraymond24@gmail.com>
2026-06-25 11:13:32 +0100

    json encoding golang

--- deepseek/json/json.go
+++ deepseek/json/json.go
@@ -1,12 +1,28 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 type ProGuy struct {
-	Name     string `json:"name"`
-	Bio      string `json:"bio"`
-	Stack    string `json:"stack"`
-	Location string `json:"location"`
+	Name     string   `json:"name"`
+	Bio      string   `json:"bio"`
+	Stack    []string `json:"stack"`
+	Location string   `json:"location"`
 }
 
-type Person struct{}
-
-func main()
+func main() {
+	p := ProGuy{
+		Name:     "Raymond",
+		Bio:      "Founder Crydensync",
+		Stack:    []string{"Go", "Git", "Js", "Bash"},
+		Location: "Remote",
+	}
+	jdata, err := json.MarshalIndent(p, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("JSON:", string(jdata))
+}
