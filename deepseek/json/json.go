34d21115f25aaa407c6294c244d2983d435e2086
rnichola <devraymond24@gmail.com>
2026-06-25 22:26:42 +0100

    fix

--- deepseek/json/json.go
+++ deepseek/json/json.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-)
-
 type ProGuy struct {
 	Name     string   `json:"name"`
 	Bio      string   `json:"bio"`
@@ -13,31 +7,6 @@ type ProGuy struct {
 	Location string   `json:"location"`
 }
 
-func main() {
-	// Encoding Go Struct to JSON
-	p := ProGuy{
-		Name:     "Raymond",
-		Bio:      "Founder Crydensync",
-		Stack:    []string{"Go", "Git", "Js", "Bash"},
-		Location: "Remote",
-	}
-	jdata, err := json.MarshalIndent(p, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("JSON:", string(jdata))
+type Person struct{}
 
-	// Decoding JSON struct to Go struct
-	jstr := `{
-		"name": "Raymond", 
-		"bio": "Founder Crydensync",
-		"stack": ["Go", "Git", "Js", "Bash"],
-		"location": "Remote"
-	}`
-	var person ProGuy
-	err = json.Unmarshal([]byte(jstr), &person)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Decoded: %+v\n", person)
-}
+func main()
