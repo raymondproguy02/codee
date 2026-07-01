4ed206fe3b01f5a0241f53d8aa09afef697f41a2
oagu <devraymond24@gmail.com>
2026-07-01 23:53:05 +0100

    fix json keys and values

--- l2e/ascii-web/main.go
+++ l2e/ascii-web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ascii-web/handlers"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -18,12 +19,18 @@ func health(w http.ResponseWriter, r *http.Request) {
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{
+	data := map[string]string{
+		"project":     "01Edu Project",
 		"name":        "Ascii Art Web",
 		"version":     "1.0.0",
-		"description": "Web version of aascii art!",
+		"description": "Web version of ascii art!",
 		"author":      "raymondproguy",
-	})
+	}
+
+	fJson, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Fprintln(w, string(fJson))
+	//json.NewEncoder(w).Encode(string(fJson))
+	
 }
 
 func main() {
