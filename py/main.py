7119dd4280269e3f2aa7d7d23c800399376725ee
oagu <devraymond24@gmail.com>
2026-07-08 01:22:39 +0100

    python calc

--- py/main.py
+++ py/main.py
@@ -1,4 +1,25 @@
 def me():
     print("Let's get the python hack started!")
-    
+
 me()
+
+# Python Calculator Mini Program
+
+opt = input("Enter operator (+ - * /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if opt == "+":
+    res = round(num1 + num2)
+    print(res)
+elif opt == "-":
+    res = round(num1 - num2)
+    print(res)
+elif opt == "*":
+    res = round(num1 * num2)
+    print(res)
+elif opt == "/":
+    res = round(num1 / num2)
+    print(res)
+else :
+    print(f"{opt} not valid operator.")
